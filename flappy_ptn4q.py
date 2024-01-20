43f2c08fed01c80f9cdda6cec91e4c6ee64c26e5
ptn4q <Narek.torosyan.new@mail.ru>
2024-01-21 01:12:02 +0500

    добавил меню, прогрев перед введением новых уровней - ???

--- flappy_ptn4q.py
+++ flappy_ptn4q.py
@@ -8,10 +8,30 @@ game_image = {}
 display_screen_window = pygame.display.set_mode((scr_width, scr_height))
 play_ground = scr_height * 0.8
 player = 'data/ptn4q.png'
-bcg = 'data/bcg_city.png'
+bcg = 'data/menu.png'
+menu = True
+level1 = False
+level2 = False
+level3 = False
 
 
-def gameplay():
+def start():
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            pygame.quit()
+        else:
+            display_screen_window.blit(game_image['background'], (0, 0))
+            pygame.display.update()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.pos[0] >= 79 and event.pos[0] <= 210:
+                    if event.pos[1] >= 214 and event.pos[1] <= 247:
+                        return 1
+                    elif event.pos[1] >= 286 and event.pos[1] <= 319:
+                        return 2
+                    elif event.pos[1] >= 358 and event.pos[1] <= 391:
+                        return 3
+
+
+def gameplay(bcg):
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
@@ -20,11 +40,10 @@ def gameplay():
         else:
             p_x = int(scr_width / 5)
             p_y = int((scr_height - game_image['player'].get_height()) / 2)
-            display_screen_window.blit(game_image['background'], (0, 0))
+            display_screen_window.blit(pygame.image.load(bcg).convert(), (0, 0))
             display_screen_window.blit(game_image['player'], (p_x, p_y))
             pygame.display.update()
             time_clock.tick(FPS)
-
 
 
 if __name__ == "__main__":
@@ -36,6 +55,17 @@ if __name__ == "__main__":
     game_image['player'] = pygame.image.load(player).convert_alpha()
 
 
-    while True:
-        gameplay()
+    while menu == True:
+        if start() == 1:
+            level1 = True
+            while level1 == True:
+                gameplay('data/bcg_city.png')
+        if start() == 2:
+            level2 = True
+            while level2 == True:
+                gameplay('data/bcg_city.png')
+        if start() == 2:
+            level3 = True
+            while level3 == True:
+                gameplay('data/bcg_city.png')
 
